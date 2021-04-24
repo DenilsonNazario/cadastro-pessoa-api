d4fd99aee8bcea51d5d063066159d77ce5c05f87
DenilsonNazario <46694628+DenilsonNazario@users.noreply.github.com>
2021-04-23 23:57:06 -0300

    Testes unitarios em PessoaController

---
+++ src/test/java/com/cadastro/pessoa/api/CadastroPessoaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cadastro.pessoa.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroPessoaApiApplicationTests {
+
+	@Test
+	public void test() {}
+
+
+}

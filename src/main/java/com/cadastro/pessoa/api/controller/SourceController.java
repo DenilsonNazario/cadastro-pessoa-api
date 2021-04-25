535c5c4b169cdbd4a0bcb9122d3ecfc52d7e7cae
DenilsonNazario <46694628+DenilsonNazario@users.noreply.github.com>
2021-04-24 23:24:23 -0300

    implementação do endpoint /source com link do projeto

---
+++ src/main/java/com/cadastro/pessoa/api/controller/SourceController.java
@@ -0,0 +1,41 @@
+package com.cadastro.pessoa.api.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.cadastro.pessoa.api.models.PessoaModel;
+import com.cadastro.pessoa.api.models.SourceModel;
+import com.cadastro.pessoa.api.repository.PessoaRepository;
+import com.cadastro.pessoa.api.repository.SourceRepository;
+
+
+
+@RestController // anotação para dizer explicidamente que esta e uma classe de controle
+@RequestMapping("/api") // uri para acessar os metodos da classe
+//@Api(value = "API Cadastro de Pessoa")
+@CrossOrigin(origins = "*") // aceitar requisições de todos os dominios
+public class SourceController {
+	
+	@Autowired // auto criar instancias
+	private SourceRepository sourceRepository;
+	
+	
+	@GetMapping("v1/source")
+	//@ApiOperation(value = "Retorna uma lista de links local e remoto do projeto.")
+	public ResponseEntity<List<SourceModel>> getPessoas() {// pegar lista de autores
+		List<SourceModel> lista = sourceRepository.findAll();
+
+		if (lista.isEmpty()) {// caso a lista estiver vazia, retorna não existe
+			return ResponseEntity.notFound().build();
+
+		}
+		return ResponseEntity.ok(lista);
+	}
+
+}
---
+++ src/main/java/com/cadastro/pessoa/api/models/SourceModel.java
@@ -0,0 +1,85 @@
+package com.cadastro.pessoa.api.models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+@Entity
+public class SourceModel {
+
+	@Id//auto increment no banco de dados
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	@JsonIgnore
+	private Long id;
+	
+	
+	private String descricao;
+	
+	private String link;
+
+	public SourceModel(Long id, String descricao, String link) {
+		super();
+		this.id = id;
+		this.descricao = descricao;
+		this.link = link;
+	}
+	
+	public SourceModel() {}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public String getLink() {
+		return link;
+	}
+
+	public void setLink(String link) {
+		this.link = link;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		SourceModel other = (SourceModel) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+	
+	
+	
+	
+	
+}
